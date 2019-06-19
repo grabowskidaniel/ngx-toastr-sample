3c5cf5e231055c4964965d2c137c2ddd4ae72359
Daniel <daniel.grabowski1@gmail.com>
2019-06-19 13:32:52 -0300

    adicionar configurações prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// Se alguma propriedade for alterada, colocar seu valor padrão em um comentário
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true, // default: false
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'avoid'
+};
