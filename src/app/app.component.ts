818f3bbc8b1d009a539c68812f758f541f0666d2
Daniel <daniel.grabowski1@gmail.com>
2019-06-20 11:41:41 -0300

    separar notification service em pasta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NotificationService } from './notification.service';
+import { NotificationService } from './notification/notification.service';
 
 @Component({
   selector: 'app-root',
