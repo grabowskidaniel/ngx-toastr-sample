80904f6093eb3aab96742c442fb411911a125412
Daniel <daniel.grabowski1@gmail.com>
2019-06-20 11:24:13 -0300

    remover ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbButtonsModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 const declarations = [AppComponent];
 
-const imports = [BrowserModule, NgbButtonsModule];
+const imports = [BrowserModule];
 
 const providers = [];
 
